a1f2ccf3ba9b358bc68ca15a8080de47d49be697
adigoppulasanjana13-eng <adigoppulasanjana13@gmail.com>
2026-06-20 14:04:14 +0530

    clean up temporary environment routes

--- backend/server.js
+++ backend/server.js
@@ -63,6 +63,7 @@ app.use('/api', async (req, res, next) => {
 });
 
 // API Routes
+
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
